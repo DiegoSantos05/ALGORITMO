3de511d4da373681a69603183ed1a6582759266a
DiegoSantos05 <di_rocha2002@icloud.com>
2024-09-19 21:21:38 -0300

    Add files via upload

---
+++ diegoquintanoite/codigo3.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main (int argc, char*argv[]){
+	int matA[2][3], mat8[2][3], matC[2][3], i, j;
+	for (i=0;<2;i++) {
+		for(j=0;j<3;j++) {
+			printf("Digire um Valor para o %do. elemento da linha %d, clouna %d da matriz A:",j+1,i+1,j+1):
+				scanf("%d",&matA[i][j]);
+		}
+	}
+	printf("\n");
+	for(i=0;i<2;i++) {
+		for(j=0;j<3;j++) {
+			printf ("Digite um valor para o %do. elemento da linha %d, coluna%d da Matriz B:",j+1,i+1,j+1);
+			scanf("%d",&matB[i][j]);
+		}
+	}
+	printf("\n Os valores somados sao: \n");
+}
+printf("\n");
+for(i=0;i<2;i++) {
+	for(j=0;j<3;j++)
+	printf("Digite um valor para o %do. elemento da linha%d, coluna %d da Matriz B:",j+1,i+1,j+1):
+		scanf("%d",&matB[i][j]);
+	}
+}
+printf("\n Os valores somados sao: \n");
+for (i=0,i<2;i++){
+	for (j=0;j<3;j++)
+	matC[i][j]*2 + matB[]
+}
